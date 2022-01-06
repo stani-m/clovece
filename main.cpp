5cebda3f5f0fd89991392b9b59e9f4cdcbbbd3e5
ado <dorot@gmail.com>
2022-01-06 19:43:29 +0100

    implemented connection to local and remote hosts

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ const int SCREEN_HEIGHT = 704;
 
 typedef struct Data {
     SDL_Renderer *renderer;
+    int numberOfPlayers;
+    std::string serverAddress;
     pthread_mutex_t *mutex;
     bool serverStarted;
     pthread_cond_t *serverStartedCond;
@@ -25,8 +27,7 @@ void *serverThread(void *arg) {
     pthread_mutex_unlock(data->mutex);
     pthread_cond_signal(data->serverStartedCond);
 
-    game.startGame(1);
-
+    game.startGame(data->numberOfPlayers);
 
     return nullptr;
 }
@@ -40,64 +41,114 @@ void *clientThread(void *arg) {
     while (!data->serverStarted) {
         pthread_cond_wait(data->serverStartedCond, data->mutex);
     }
-    client.start("localhost", 1234);
+    client.start(data->serverAddress, 1234);
     pthread_mutex_unlock(data->mutex);
-
 
     return nullptr;
 }
 
-int main() {
+std::pair<SDL_Window *, SDL_Renderer *> createRenderer() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
+        return {nullptr, nullptr};
     }
 
     SDL_Window *window = SDL_CreateWindow("Človeče nehnevaj sa!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                           SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
-        return 1;
+        return {nullptr, nullptr};
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr) {
         fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
+        return {nullptr, nullptr};
+    }
+    return {window, renderer};
+}
+
+int main(int argn, char *argv[]) {
+    if (argn < 3) {
+        fprintf(stderr, "incorrect number of arguments\n");
         return 1;
     }
 
-    srand(time(nullptr));
+    if (std::string(argv[1]) == "start") {
+        auto[window, renderer] = createRenderer();
+        if (renderer == nullptr) {
+            return 1;
+        }
 
-    pthread_t server, client;
+        srand(time(nullptr));
 
-    pthread_mutex_t mutex;
-    pthread_cond_t serverStartedCond;
+        pthread_t server, client;
 
-    pthread_mutex_init(&mutex, nullptr);
-    pthread_cond_init(&serverStartedCond, nullptr);
+        pthread_mutex_t mutex;
+        pthread_cond_t serverStartedCond;
 
-    Data data{
-            .renderer = renderer,
-            .mutex = &mutex,
-            .serverStarted = false,
-            .serverStartedCond = &serverStartedCond
-    };
+        pthread_mutex_init(&mutex, nullptr);
+        pthread_cond_init(&serverStartedCond, nullptr);
 
-    pthread_create(&server, nullptr, &serverThread, &data);
-    pthread_create(&client, nullptr, &clientThread, &data);
+        Data data{
+                .renderer = renderer,
+                .numberOfPlayers = atoi(argv[2]),
+                .serverAddress = "localhost",
+                .mutex = &mutex,
+                .serverStarted = false,
+                .serverStartedCond = &serverStartedCond
+        };
 
+        pthread_create(&server, nullptr, &serverThread, &data);
+        pthread_create(&client, nullptr, &clientThread, &data);
 
-    pthread_join(client, nullptr);
-    pthread_join(server, nullptr);
+        pthread_join(client, nullptr);
+        pthread_join(server, nullptr);
 
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&serverStartedCond);
+        pthread_mutex_destroy(&mutex);
+        pthread_cond_destroy(&serverStartedCond);
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
 
-    SDL_Quit();
+        SDL_Quit();
+    } else if (std::string(argv[1]) == "connect") {
+        auto[window, renderer] = createRenderer();
+        if (renderer == nullptr) {
+            return 1;
+        }
 
+        srand(time(nullptr));
+
+        pthread_t client;
+
+        pthread_mutex_t mutex;
+        pthread_cond_t serverStartedCond;
+
+        pthread_mutex_init(&mutex, nullptr);
+        pthread_cond_init(&serverStartedCond, nullptr);
+
+        Data data{
+                .renderer = renderer,
+                .numberOfPlayers = -1,
+                .serverAddress = std::string(argv[2]),
+                .mutex = &mutex,
+                .serverStarted = true,
+                .serverStartedCond = &serverStartedCond
+        };
+
+        pthread_create(&client, nullptr, &clientThread, &data);
+
+        pthread_join(client, nullptr);
+
+        pthread_mutex_destroy(&mutex);
+        pthread_cond_destroy(&serverStartedCond);
+
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+
+        SDL_Quit();
+    }
     return 0;
 }
